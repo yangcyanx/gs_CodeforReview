5417c1174db3dcf7a23ec6aae9d68e5d7ec92ff3
yxy <3230604009@stu.cuit.edu.cn>
2025-08-21 18:07:57 +0800

    update

--- generate_normals_depths_shinyReal.sh
+++
@@ -1,6 +0,0 @@
-python generate_depths.py --input_dir data/shiny_real/gardenspheres/images_4
-python generate_normals.py --input_dir data/shiny_real/gardenspheres/images_4
-python generate_depths.py --input_dir data/shiny_real/sedan/images_8
-python generate_normals.py --input_dir data/shiny_real/sedan/images_8
-python generate_depths.py --input_dir data/shiny_real/toycar/images_4
-python generate_normals.py --input_dir data/shiny_real/toycar/images_4
